21315054efaef1c0a575a6d187ea7406f171a9bf
Hanna Murai <57275209+murai1998@users.noreply.github.com>
2020-08-04 17:26:32 -0500

    Add files via upload

---
+++ Delete.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
+
+class Delete extends Component {
+    render() {
+        return (
+            <Router>
+            <a href="https://trupanion.com/">Help now</a>
+            </Router>
+        );
+    }
+}
+
+export default Delete;
